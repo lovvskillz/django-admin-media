35295ac283fba9ae4baea420d553841d79c71f17
lovvskillz <14542790+lovvskillz@users.noreply.github.com>
2023-07-21 14:29:05 +0200

    lint code

--- media_preview/apps.py
+++ media_preview/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MediaPreviewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'media_preview'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media_preview"
